dc1f012bb6643f25565fbceb2739481090f0b889
krystal gates <krystal_sabrina@yahoo.com>
2019-07-29 13:11:51 -0500

    set up app views finish login

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class DestintationList extends Component {
+export default class LocationList extends Component {
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
